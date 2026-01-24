33b24e66020c3e11191436575d433ecdcc17864f
Bot <test@test.com>
2026-01-24 01:43:14 +0100

    Fix Render port binding to use environment variable

--- api_server.py
+++ api_server.py
@@ -1700,4 +1700,5 @@ if __name__ == '__main__':
     # Start WebSocket stream
     start_websocket_stream()
     
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
